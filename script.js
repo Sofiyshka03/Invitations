b1bc07f051bebf720ffa29f0cdc1b5d122f1e950
Suniyatullina Safia Faridovna <137382347+Sofiyshka03@users.noreply.github.com>
2024-11-04 17:32:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Alert to simulate purchase
+document.querySelectorAll('.buy-now').forEach(button => {
+    button.addEventListener('click', () => {
+        alert("Thank you for your purchase!");
+    });
+});
